a761b9f6f3aac85383734e0df8997868b57b9fc8
Millard Harris <millard@freemountaininc.com>
2023-09-17 20:12:43 -0700

    End of section 12

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -8,6 +8,11 @@ namespace API.Services
 {
     public class TokenService
     {
+        private readonly IConfiguration _config;
+        public TokenService(IConfiguration config)
+        {
+            _config = config;
+        }
         public string CreateToken(AppUser user)
         {
             var claims = new List<Claim>
@@ -17,7 +22,7 @@ namespace API.Services
                 new Claim(ClaimTypes.Email, user.Email),
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("rQXCF2aMua5nKSy8UZmuzM7YFukKd62bWr6Udcwxq2PL53MTZUTwdKmrmq2ASQNh"));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
 
             var tokenDescriptor = new SecurityTokenDescriptor
