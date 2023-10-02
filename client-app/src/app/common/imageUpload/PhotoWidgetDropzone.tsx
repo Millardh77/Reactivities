b3bece8a432fe49fa0abcb24daa61738eb535613
Millard Harris <millard@freemountaininc.com>
2023-10-02 09:20:55 -0700

    Committing for review

--- client-app/src/app/common/imageUpload/PhotoWidgetDropzone.tsx
+++ client-app/src/app/common/imageUpload/PhotoWidgetDropzone.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react'
+import {useCallback} from 'react'
 import {useDropzone} from 'react-dropzone'
 import { Header, Icon } from 'semantic-ui-react';
 
@@ -6,32 +6,33 @@ interface Props {
     setFiles: (files: any) => void;
 }
 
-export default function PhotoWidgetDropzone({setFiles}: Props) {
+export default function PhotoUploadWidgetDropzone({setFiles}: Props) {
     const dzStyles = {
         border: 'dashed 3px #eee',
         borderColor: '#eee',
-        borderRadiou: '5px',
+        borderRadius: '5px',
         paddingTop: '30px',
         textAlign: 'center' as 'center',
-        height: 200
+        height: '200px'
     }
 
     const dzActive = {
-        borderColor: 'green'
+        borderColor: 'green',
     }
 
-  const onDrop = useCallback(acceptedFiles => {
-    setFiles(acceptedFiles.map((file: any) => Object.assign(file, {
-        preview: URL.createObjectURL(file)
-    })))
-  }, [setFiles])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    const onDrop = useCallback((acceptedFiles: any) => {
+        setFiles(acceptedFiles.map((file: any) => Object.assign(file, {
+            preview: URL.createObjectURL(file)
+        })))
+    }, [setFiles])
 
-  return (
-    <div {...getRootProps()} style={isDragActive ? {...dzStyles, ...dzActive} : dzStyles}>
-      <input {...getInputProps()} />
-      <Icon nae='upload' siz='huge' />
-      <Header content='Drop image here' />
-    </div>
-  )
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+    return (
+        <div {...getRootProps()} style={isDragActive ? {...dzStyles, ...dzActive} : dzStyles}>
+            <input {...getInputProps()} />
+            <Icon name='upload' size='huge' />
+            <Header content='Drop image here' />
+        </div>
+    )
 }
